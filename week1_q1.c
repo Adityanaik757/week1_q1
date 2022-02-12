d5e1c00d2b7d38022db5e2fb7e403dd59ac567db
Adityanaik757 <99343157+Adityanaik757@users.noreply.github.com>
2022-02-12 20:41:33 +0530

    Liftoff C Assignment

---
+++ week1_q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main()
+
+ { printf("Name : Aditya Naik\n"); 
+   printf("Regd.No : 003\n"); 
+   printf("Branch : Civil\n");
+   printf("Hobbies : Playing cricket\n");
+
+return(0); }
